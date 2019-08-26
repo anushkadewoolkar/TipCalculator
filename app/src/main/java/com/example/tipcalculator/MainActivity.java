15914e6377977f1303efe08b633898db9ac33308
anushkadewoolkar <dewoolkar6949@students.d211.org>
2019-08-25 22:11:57 -0500

    FINAL PROJECT - DEWOOLKAR

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -18,14 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        ConstraintLayout constraintLayout = findViewById(R.id.layout);
-//        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
-//        animationDrawable.setEnterFadeDuration(2000);
-//        animationDrawable.setExitFadeDuration(4000);
-//        animationDrawable.start();
+        ConstraintLayout constraintLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
     }
 
     public void calculate(View v){
+        DecimalFormat df = new DecimalFormat("$#.00");
         EditText bill = (EditText) findViewById(R.id.editText);
         EditText tip = (EditText) findViewById(R.id.editText2);
         EditText percent = (EditText) findViewById(R.id.editText2);
@@ -38,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         double addedBill = billNumber * (tipNumber/100);
         double totalBill = addedBill + billNumber;
 
-        finalBill.setText("Final Bill: " + totalBill);
+        finalBill.setText("Final Bill: " + df.format(totalBill));
 
     }
 }
